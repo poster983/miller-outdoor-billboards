ad144447619653c46e35648f143d84dce70b6edc
Glitch (miller-outdoor-billboards) <none>
2019-07-26 23:34:38 +0000

    Added Google Analytics

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const axios = require('axios');
 const mustacheExpress = require('mustache-express');
 const { DateTime } = require("luxon");
 const mockData = require("./darksky-mock-data.json");
+const uuid = require('uuid/v4');
+const cookieParser = require('cookie-parser');
+const QRCode = require('qrcode')
+
 require('dotenv').config();
 
 // Register '.mustache' extension with The Mustache Express
@@ -15,6 +19,7 @@ app.engine('html', mustacheExpress());
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(cookieParser());
 
 
 // we've started you off with Express, 
@@ -48,6 +53,7 @@ let queryDarksky = () => { //Update dark sky
   } else {
     return resolve(darkSkyCache.data)
   }
+    //return resolve(mockData)
   })
 }
 
@@ -94,7 +100,6 @@ app.get('/weather', function(request, response) {
   
   queryDarksky()
       .then(() => {
-        console.log(darkSkyCache.lastUpdated.toISO())
         respond();
       }).catch((err) => {
         response.render('error', {
@@ -115,7 +120,6 @@ app.get('/weather', function(request, response) {
 app.get('/radar', function(request, response) {
   queryDarksky()
       .then((data) => {
-        console.log(darkSkyCache.lastUpdated.toISO())
         console.log(data.currently.nearestStormDistance)
         if(data.currently.nearestStormDistance <= 15) { // if the nearest storm is within 15 miles, then show radar
           return response.render('radar', {
@@ -131,6 +135,47 @@ app.get('/radar', function(request, response) {
       })
  
 });
+
+app.get('/alerts', function(req, res) {
+  let GA_ID=null;
+  if(!req.cookies.GA) {
+    GA_ID = uuid();
+    res.cookie('GA', GA_ID, {expires: DateTime.utc().plus({years: 2}).toJSDate()});
+
+  } else {
+    GA_ID = req.cookies.GA;
+  }
+  
+  console.log(GA_ID);
+  
+  axios.post(`https://www.google-analytics.com/collect?v=1&t=pageview&tid=${process.env.GA_TRACKING_ID}&cid=${GA_ID}&dp=%2Falerts`).then((response) => {
+    console.log("/alerts:", "Google Analytics ")
+  }).catch((err)=>{
+    console.error("/alerts:", err);
+  })
+  
+  return res.redirect("https://darksky.net/forecast/29.7196,-95.389/us12/en");
+})
+
+//generates qrcode that links to /alerts
+app.get('/alerts/qr.png', function(req, res) {
+  var baseURL = req.get('host');
+  //console.log("/alerts/qr.png:", baseURL)
+  
+  QRCode.toDataURL(`http://${baseURL}/alerts`, { errorCorrectionLevel: 'H', width: "250"}, function (err, url) {
+    if(err) {
+      console.error("/alerts/qr.png:", err)
+    }
+    //console.log(url)
+    const img = Buffer.from(url.split(",")[1], 'base64');
+    res.writeHead(200, {
+     'Content-Type': 'image/png',
+     'Content-Length': img.length
+     });
+    res.end(img);
+  })
+
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
